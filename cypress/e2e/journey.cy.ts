0f07093484d4f560ef977ee6eecf18163edc06bf
Abdullah Ansari <aa9272137@gmail.com>
2023-06-16 17:13:23 +0400

    chore: update journey.cy.ts

--- cypress/e2e/journey.cy.ts
+++ cypress/e2e/journey.cy.ts
@@ -1,5 +1,5 @@
 describe(("subscribe form test"), () => {
-    it("user finds a course on a homepage and can complete the course by following the steps", () => {
+    it("user finds the first course on a homepage and can complete the course by following the steps", () => {
         cy.visit("http://localhost:3000");
         cy.getByDataTest("course-0").find("a").eq(-1).click();
         cy.location("pathname")
@@ -14,6 +14,22 @@ describe(("subscribe form test"), () => {
         cy.location("pathname")
         .should("eq", "/testing-your-first-application/installing-cypress-and-writing-our-first-test");
 
-        
+    })
+    
+    it("user finds the first course on a homepage and can complete the course by following the steps", () => {
+        cy.visit("http://localhost:3000");
+        cy.getByDataTest("course-0").find("a").eq(-1).click();
+        cy.location("pathname")
+        .should("eq", "/testing-your-first-application");
+
+        cy.getByDataTest("next-lesson-button").click();
+        cy.location("pathname")
+        .should("eq", "/testing-your-first-application/app-install-and-overview");
+
+        cy.getByDataTest("challenge-answer-0").click();
+        cy.getByDataTest("next-lesson-button").should("exist").click();
+        cy.location("pathname")
+        .should("eq", "/testing-your-first-application/installing-cypress-and-writing-our-first-test");
+
     })
 });
